14e133d8a0e4b1b33f9df3d7ec1be93f0d809437
ivan.corrales <ivan.corrales@bbva.com>
2017-05-30 19:57:33 +0200

    feat: adding travis configuration and fixing lint issues

--- database/database.go
+++ database/database.go
@@ -1,29 +1,30 @@
 package database
 
 import (
-	"github.com/swatlabs/GoDataberus/data_model"
+	"github.com/swatlabs/GoDataberus/datamodel"
 )
 
-
+//ConnectionDB struct
 type ConnectionDB struct {
-	DbProto         string `json:"DbProto"`
-	DbIpaddress  	string `json:"DbIpaddress"`
-	DbPort          string `json:"DbPort"`
-	DbName		string `json:"DbName"`
-	DbUsername	string `json:"DbUsername"`  //this is optional if the type of database requires auth
-	DbPassword      string `json:"DbPassword"`  //this is optional if the type of database requires auth
-	DbCollection    string `json:"DbCollection"`  //this is optional just in case that we use mongodb
+	DbProto      string `json:"DbProto"`
+	DbIpaddress  string `json:"DbIpaddress"`
+	DbPort       string `json:"DbPort"`
+	DbName       string `json:"DbName"`
+	DbUsername   string `json:"DbUsername"`   //this is optional if the type of database requires auth
+	DbPassword   string `json:"DbPassword"`   //this is optional if the type of database requires auth
+	DbCollection string `json:"DbCollection"` //this is optional just in case that we use mongodb
 }
 
-type BodyRequest struct{
-	Connection	ConnectionDB `json:"DBconnection"`
-	Message 	data_model.Information `json:"data"`
+//BodyRequest struct
+type BodyRequest struct {
+	Connection ConnectionDB          `json:"DBconnection"`
+	Message    datamodel.Information `json:"data"`
 }
 
+//Store interface
 type Store interface {
 	Initialize(c *ConnectionDB) error
-	InsertEntity(i *data_model.Information) error
-	IsNew(field,searchItem string) bool
-	GetEntity(field,searchItem string) ([]data_model.Information, error)
+	InsertEntity(i *datamodel.Information) error
+	IsNew(field, searchItem string) bool
+	GetEntity(field, searchItem string) ([]datamodel.Information, error)
 }
-
