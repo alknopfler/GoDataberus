db386344883804aa58e8323ee6ab906f3427e661
alberto.morgante <alberto.morgante@bbva.com>
2017-05-30 11:25:54 +0200

    insert item finished

--- api/api.go
+++ api/api.go
@@ -9,7 +9,6 @@ import (
 	"github.com/swatlabs/GoDataberus/redis"
 	"github.com/swatlabs/GoDataberus/database"
 	"github.com/garyburd/redigo/redis"
-	"fmt"
 )
 
 //HandlerController function
@@ -35,8 +34,8 @@ func HandlerCheckConnections(w http.ResponseWriter, r *http.Request) {
 			responseWithError(w,http.StatusServiceUnavailable,err.Error())
 		}else{
 			uuid:=utils.NewResourceID()
-			encoded,_:=json.Marshal(db.Connection)
-			(redisDB.NewRedis()).Do("LPUSH",uuid,encoded)
+			encode,_:=json.Marshal(db.Connection)
+			(redisDB.NewRedis()).Do("LPUSH",uuid,encode)
 			responseWithJSON(w,http.StatusCreated,uuid)
 		}
 	}
@@ -50,14 +49,13 @@ func HandlerInsert(w http.ResponseWriter, r *http.Request){
 	drv:=utils.GetDriver(dbType)
 
 	dbconnect,_:=redis.Strings((redisDB.NewRedis()).Do("LRANGE",uuid,0,-1))
-	fmt.Println(dbconnect[0])
 	json.Unmarshal([]byte(dbconnect[0]), &unencoded.Connection)
-	fmt.Println(unencoded.Connection)
 
 	if err:=drv.Initialize(&unencoded.Connection);  err!=nil{
 		responseWithError(w,http.StatusServiceUnavailable,err.Error())
 	}else{
-		responseWithJSON(w,200,unencoded.Connection)
+		input,_:=utils.GetDataFromBody(r)
+		drv.InsertEntity(&input.Message)
 	}
 
 
